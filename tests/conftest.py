671a334d87650e4c86a47f3f3f793dcf9e4a5be0
hartleyn <nhartley18@me.com>
2019-08-05 17:33:02 -0400

    Added setup.py and TwitchClient unit tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,15 @@
 import pytest
-from app import create_app
+from app import create_app, twitch_operations
 
 
 @pytest.fixture
-def client():
+def flask_app_client():
   app = create_app()
   with app.test_client() as client:
     client.testing = True
     yield client
+
+@pytest.fixture(scope='module')
+def twitch_client():
+  client = twitch_operations.TwitchClient()
+  return client
--- tests/test_flask_app.py
+++ tests/test_flask_app.py
@@ -1,7 +1,4 @@
-import pytest
 
-
-def test_fetch_twitch_user_id(client):
-  res = client.get('/timeline/nick_lenoir')
+def test_fetch_twitch_user_id(flask_app_client):
+  res = flask_app_client.get('/timeline/nick_lenoir')
   assert res.status_code == 200
-  
