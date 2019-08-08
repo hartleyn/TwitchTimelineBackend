3b7639fa30196d4df054db5c01066091b8685110
hartleyn <nhartley18@me.com>
2019-08-08 17:14:08 -0400

    Added duration calculation to timeline endpoint.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, make_response
 from flask_cors import CORS
 from .twitch_operations import TwitchClient
+from .helpers import convert_date_string_to_datetime, build_follow_duration_string
 
 
 def create_app():
@@ -16,6 +17,11 @@ def create_app():
   def fetch_twitch_timeline(username):
     user_id = twitch_client.fetch_user_id(username)
     follow_list = twitch_client.fetch_user_follow_list(user_id)
+    for user in follow_list:
+      user['follow_duration'] = build_follow_duration_string(user['followed_at'])
+      user['followed_at'] = convert_date_string_to_datetime(user['followed_at']).__str__()
+    # Earliest follow first
+    follow_list.reverse()
     res = make_response({'data': {'total': len(follow_list), 'follow_list': follow_list}}, 200)
     return res
   
