35bb0d7d1c44f763163d423473aa590bdf4b3bc1
hartleyn <nhartley18@me.com>
2019-08-08 17:14:33 -0400

    Created helpers.py

--- app/helpers.py
+++ app/helpers.py
@@ -16,3 +16,23 @@ def calculate_follow_duration(followed_at_datetime):
   duration = datetime.now() - followed_at_datetime
   duration = duration.__str__().split('.')[0]
   return duration
+
+def convert_days_to_years_months_days(days):
+  years = days // 365
+  days = days % 365
+  months = int(days / 30.44)
+  days = int(days % 30.44)
+  return (years, months, days)
+
+def build_follow_duration_string(followed_at):
+  followed_at_dt = convert_date_string_to_datetime(followed_at)
+  duration = calculate_follow_duration(followed_at_dt)
+  duration_str = ''
+  if 'days' in duration:
+    days = int(duration.split()[0])
+    ymd = convert_days_to_years_months_days(days)
+    duration_str = f'{ymd[0]} years, {ymd[1]} months, {ymd[2]} days, '
+  time = duration.split()[-1]
+  time = [int(num) for num in time.split(':')]
+  duration_str += f'{time[0]} hours, {time[1]} minutes, {time[2]} seconds'
+  return duration_str
