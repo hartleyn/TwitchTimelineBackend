ac8042544652e68d05ec67e4a1b5c73023885403
hartleyn <nhartley18@me.com>
2019-11-04 01:03:59 -0500

    Added test for _build_url_parameter_string helper.

--- tests/test_twitch_operations.py
+++ tests/test_twitch_operations.py
@@ -1,3 +1,4 @@
+import pytest
 from app.twitch_operations import TwitchClient
 
 
@@ -13,5 +14,33 @@ def test_fetch_user_follow_list(twitch_client):
 
 def test_reset_user(twitch_client):
   twitch_client.fetch_user_follow_list('59156455')
-  twitch_client.reset_user()
+  twitch_client._reset_user()
   assert len(twitch_client.user_follow_list) == 0
+
+@pytest.mark.parametrize('key_in_string, value_list, key_in_list, expected_string', (
+  (
+    'user_id', 
+    (
+      {'from_id': '59156455', 'from_name': 'nick_lenoir', 'to_id': '195166073', 'to_name': 'HAchubby', 'followed_at': '2019-09-19T05:17:16Z'},
+      {'from_id': '59156455', 'from_name': 'nick_lenoir', 'to_id': '79122704', 'to_name': 'Javion', 'followed_at': '2019-09-12T08:44:40Z'},
+      {'from_id': '59156455', 'from_name': 'nick_lenoir', 'to_id': '52326616', 'to_name': 'Sunny', 'followed_at': '2019-09-04T12:26:18Z'},
+      {'from_id': '59156455', 'from_name': 'nick_lenoir', 'to_id': '193785576', 'to_name': '콩만한정은이', 'followed_at': '2019-08-30T13:08:55Z'},
+      {'from_id': '59156455', 'from_name': 'nick_lenoir', 'to_id': '157970655', 'to_name': 'Myrilliaa', 'followed_at': '2019-08-28T18:15:39Z'},
+    ), 
+    'to_id',
+    'user_id=195166073&user_id=79122704&user_id=52326616&user_id=193785576&user_id=157970655',
+  ),
+  (
+    'id',
+    (
+      '101',
+      '202',
+      '303',
+    ),
+    None,
+    'id=101&id=202&id=303',
+  ),
+))
+def test_build_url_parameter_string(twitch_client, key_in_string, value_list, key_in_list, expected_string):
+  url_paramater_string = twitch_client._build_url_parameter_string(key_in_string, value_list, key_in_list)
+  assert url_paramater_string == expected_string
