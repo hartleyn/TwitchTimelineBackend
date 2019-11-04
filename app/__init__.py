5c5dcf6f13a007a5eb3ed679969dca0a9a46b810
hartleyn <nhartley18@me.com>
2019-11-04 01:02:55 -0500

    Added Twitch webhook endpoints.

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,23 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 from flask_cors import CORS
+#from flask_ask import Ask, statement, question
 from .twitch_operations import TwitchClient
 from .helpers import convert_date_string_to_datetime, build_follow_duration_string
 
+
+#ask = Ask()
 
 def create_app():
   """
   Flask app factory function.
   """
   app = Flask(__name__)
-  CORS(app, resources={r'/*': {'origins': ['http://localhost:3000', 'https://competent-hugle-41176c.netlify.com']}})
+  #ask.init_app(app)
+  CORS(app, resources={r'/*': {'origins': [
+    'http://localhost:3000', 
+    'https://competent-hugle-41176c.netlify.com', 
+    'https://api.twitch.tv/helix'
+  ]}})
 
   twitch_client = TwitchClient()
 
@@ -28,5 +36,22 @@ def create_app():
     follow_list.reverse()
     res = make_response({'data': {'total': len(follow_list), 'follow_list': follow_list}}, 200)
     return res
-  
+
+  @app.route('/webhooks/subscriber', methods=['GET', 'POST'])
+  def receive_webhook_message():
+    if request.method == 'POST':
+      print(request.data)
+      res = make_response({'success': True}, 200)
+    else:
+      challenge_token = request.args.get('hub.challenge')
+      res = make_response(challenge_token, 200)
+    return res
+
+  '''
+  @ask.launch
+  def start_skill():
+    msg = 'Hello, can I find something on Twitch for you?'
+    return question(msg)
+  '''
+
   return app
